bf01864358c65fb5053b396a28250f8ccb6bd76c
AhsanDz <ahsanfaqih6@gmail.com>
2025-11-23 01:31:53 +0700

    fix iuran

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use App\Http\Controllers\DataController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IuranController;
 
 Route::get('/warga/tambah', [DataController::class, 'createWarga']);
 Route::post('/warga', [DataController::class, 'storeWarga']);
 
-Route::get('/iuran/tambah', [DataController::class, 'createIuran']);
-Route::post('/iuran', [DataController::class, 'storeIuran']);
+Route::get('/iuran/tambah', [IuranController::class, 'createIuran']);
+Route::post('/iuran', [IuranController::class, 'storeIuran']);
